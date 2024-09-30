55f3f484c08612b52f86a979a17595b191e4ba23
kamillask <kamil.laskowski86@qmail.cuny.edu>
2024-09-30 13:54:30 -0400

    Fixed JS logic for password verification.

--- scripts.js
+++ scripts.js
@@ -1,22 +1,60 @@
 const pass = document.querySelector("#password");
 const confirmPass = document.querySelector("#confirmPass");
+const passContainer = document.querySelector("#loginForm");
 
 const submitButton = document.querySelector("#submitButton");
 
-// submitButton.addEventListener("click", () => {
-//     verifyPassword();
-// })
+const errorMessage = document.createElement("div");
+errorMessage.setAttribute("id", "errorMessage");
 
-confirmPass.addEventListener("focusout", () => {
-    verifyPassword();
-})
-
-function verifyPassword(){
-    if(pass.value===confirmPass.value){
-        pass.style.border = "1px solid green";
-        confirmPass.style.border = "1px solid green";
+function checkLength(password){
+    if(password.value.length<8){
+        return false;
     } else{
-        pass.style.border = "1px solid red";
-        confirmPass.style.border = "1px solid red";
+        return true;
     }
 }
+
+function checkMatching(){
+    if(pass.value===confirmPass.value){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+pass.addEventListener("focusout", () => {
+    if(confirmPass.value!=="" && checkMatching()===false){
+        pass.style.border = "1px solid red";
+        errorMessage.textContent = "Passwords do not match.";
+        passContainer.appendChild(errorMessage);
+        return;
+    }
+    if(checkLength(pass)===true){
+        pass.style.border = "1px solid green";
+        passContainer.removeChild(errorMessage);
+    } else{
+        pass.style.border = "1px solid red";
+        errorMessage.textContent = "Password is too short.";
+        passContainer.appendChild(errorMessage);
+    }
+    
+})
+
+confirmPass.addEventListener("focusout", () => {
+    if(checkLength(confirmPass)===false){
+        confirmPass.style.border = "1px solid red";
+        errorMessage.textContent = "Password is too short.";
+        passContainer.appendChild(errorMessage);
+        return;
+    }
+    if(checkMatching()===true){
+        confirmPass.style.border = "1px solid green";
+        pass.style.border = "1px solid green";
+        passContainer.removeChild(errorMessage);
+    } else{
+        confirmPass.style.border = "1px solid red";
+        errorMessage.textContent = "Passwords do not match.";
+        passContainer.appendChild(errorMessage);
+    }
+})
